9560f2ddcfb832d7b722a717faa94f9f1f36ddda
cristopher ontiveros <christoph.alano@gmail.com>
2024-01-04 07:10:04 -0600

    added buttons, use state and passed topic to use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  function handleSelect() {
-    
+  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'compnents', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
   }
 
   return (
@@ -24,12 +28,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>
+              JSX
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>
+              Props
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>
+              State
+            </TabButton>
           </menu>
-          
+          {selectedTopic}
         </section>
       </main>
     </div>
