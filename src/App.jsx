8e12623072ca45538233a6f5b450aa88fd3a2f23
cristopher ontiveros <christoph.alano@gmail.com>
2024-01-04 03:56:21 -0600

    destructured props

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,12 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt={props.description} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={description} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -38,10 +38,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
-            <CoreConcept {...CORE_CONCEPTS[4]} />
           </ul>
         </section>
       </main>
